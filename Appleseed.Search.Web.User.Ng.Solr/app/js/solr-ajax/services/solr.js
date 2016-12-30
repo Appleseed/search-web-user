dd397feaddedc3d41a70cdf7e3a780e3984e92cb
Rahul Singh <rahul.singh@anant.us>
2016-12-30 12:49:58 -0500

    better add/update/remove facet

--- Appleseed.Search.Web.User.Ng.Solr/app/js/solr-ajax/services/solr.js
+++ Appleseed.Search.Web.User.Ng.Solr/app/js/solr-ajax/services/solr.js
@@ -112,18 +112,23 @@ function SolrQuery(Url) {
 
     ///////////////////////////////////////////////////////////////////////////
 
+    
     /**
      * Add facet constraint if it does not already exist, or updates it if it does
      * @param Facet
      */
-    self.addFacet = function(Facet) {
+     self.addFacet = function(Facet) {
         for (var i=0;i<self.facets.length;i++) {
-            if (self.facets[i].field == Facet.field &&
+            if (self.facets[i].field == Facet.field && Facet.value == '') {
+                self.removeFacetByIndex(i);
+                return;
+            } else if (self.facets[i].field == Facet.field && Facet.value != '') {
+                self.facets[i].value = Facet.value ;
+                return;
+            } else if (self.facets[i].field == Facet.field &&
                 self.facets[i].value == Facet.value) {
                 return;
-            } else if (self.facets[i].field == Facet.field) {
-                self.facets[i].value = Facet.value ;
-            }
+            }  
         }
         self.facets.push(Facet);
     };
